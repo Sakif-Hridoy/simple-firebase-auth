3bb978dce24c953d412744d70aa1f2cbd38b4611
NFS Heart Sakif Hridoy <sakifworkspace@gmail.com>
2021-11-05 12:50:46 +0600

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,10 +1,6 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyChS8XnMbeFSIezl87MblYSQIvVavJkAXw",
-    authDomain: "ema-john-simple-20211202.firebaseapp.com",
-    projectId: "ema-john-simple-20211202",
-    storageBucket: "ema-john-simple-20211202.appspot.com",
-    messagingSenderId: "1030148482083",
-    appId: "1:1030148482083:web:3025ea00bdf493cd2abd96"
+    firebase project name "ema-john-simple"
+    
   };
 
   export default firebaseConfig;
